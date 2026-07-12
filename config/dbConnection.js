439d98d96a994f60e4db736319e874ee5b29ed1e
Adnan Khalid <109947930+Adnankhalid606@users.noreply.github.com>
2026-07-12 09:11:05 +0500

    configure environment variables

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,23 @@
 import mysql from 'mysql2/promise';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = await mysql.createPool({
-    host: 'localhost',
-    port: 3307,   
-    user: 'root',
-    password: '',
-    database: 'blogs'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,   
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 })
+
+try{
+    const connection = await db.getConnection();
+    console.log("Database Connected");
+    connection.release();
+}
+catch(err){
+    console.error('Error connecting to the database:', err);
+}
 
 export default db;
